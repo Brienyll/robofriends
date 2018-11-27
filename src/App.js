a1c70b14c6a48638233763ca38f9036288462e41
Brienyll <brienyll29@gmail.com>
2018-11-26 23:29:45 -0800

    added JSON API and mounted

--- src/App.js
+++ src/App.js
@@ -8,24 +8,36 @@ class App extends Component {
   constructor() {
     super()
     this.state = {
-      robots: robots,
+      robots: [],
       searchfield: ''
     }
   }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({ robots: users }));
+  }
+
   onSearchChange = (e) => {
     this.setState({searchfield: e.target.value});
   }
+
   render() {
     const filteredRobots = this.state.robots.filter(robots => {
       return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
     })
-    return (
-      <div className='tc'>
-        <h1> Robo Family </h1>
-        <SearchBox searchChange={this.onSearchChange}/>
-        <CardList robots={filteredRobots}/>
-      </div>
-    );
+    if (this.state.robots.length === 0) {
+      return <h1>Loading</h1>
+    } else {
+      return (
+        <div className='tc'>
+          <h1> Robo Family </h1>
+          <SearchBox searchChange={this.onSearchChange}/>
+          <CardList robots={filteredRobots}/>
+        </div>
+     );
+    }
   }
 }
 export default App;
