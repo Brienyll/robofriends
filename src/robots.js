0232de545f94291294603bc5703346f18afcf1a4
Brienyll <Brienyll29@gmail.com>
2019-06-28 12:33:23 -0700

    redux - error on filtered robots

---
+++ src/robots.js
@@ -0,0 +1,63 @@
+export const robots = [
+    {
+      id: 1,
+      name: 'Genyll Pedrigala',
+      username: 'Gen Gen',
+      email: 'Genyll@google.com'
+    },
+    {
+      id: 2,
+      name: 'Brielle Genna Angeles',
+      username: 'Brielle',
+      email: 'brielle@gmail.com.ph'
+    },
+    {
+      id: 3,
+      name: 'Brix Angeles',
+      username: 'Brix',
+      email: 'brix.angeles@gmail.com'
+    },
+    {
+      id: 4,
+      name: 'Gendick Pedrigala',
+      username: 'Gendick',
+      email: 'Gendick@kory.org'
+    },
+    {
+      id: 5,
+      name: 'Gencer Pedrigala',
+      username: 'Gencer',
+      email: 'gencer@annie.ca'
+    },
+    {
+      id: 6,
+      name: 'Corgen Entroleza',
+      username: 'Mommy',
+      email: 'Corgen@jasper.info'
+    },
+    {
+      id: 7,
+      name: 'Daddy Nats Entrolezo',
+      username: 'Daddy Nato',
+      email: 'Nato@billy.biz'
+    },
+    {
+      id: 8,
+      name: 'Gianna Elize Pedrigala',
+      username: 'Nana',
+      email: 'Elize@rosamond.me'
+    },
+    {
+      id: 9,
+      name: 'Junjun Belarmino',
+      username: 'Jun',
+      email: 'Junjun@dana.io'
+    },
+    {
+      id: 10,
+      name: 'Janjan Belarmino',
+      username: 'Jan',
+      email: 'Janjan@karina.biz'
+    }
+  ];
+  
